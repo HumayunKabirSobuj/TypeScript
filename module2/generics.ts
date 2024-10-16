0f648dd0e387b99cc9480479b5984dfdce321453
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2024-10-16 15:30:55 +0600

    Generic Added

---
+++ module2/generics.ts
@@ -0,0 +1,47 @@
+{
+    // 
+
+    // Generic Type
+
+    type GenericType<T> = Array<T>
+
+    // const rollNumbers: number[] = [1, 3, 5];
+    // const rollNumbers: Array<number> = [1, 3, 5];
+    const rollNumbers: GenericType<number> = [1, 3, 5];
+
+    // const mentors: string[] = ['Mr. X', 'Mr. Y', 'Mr. Z'];
+    // const mentors: Array<string> = ['Mr. X', 'Mr. Y', 'Mr. Z'];
+    const mentors: GenericType<string> = ['Mr. X', 'Mr. Y', 'Mr. Z'];
+
+    // const booleanArray: boolean[] = [true, false, true]
+    // const booleanArray: Array<boolean> = [true, false, true]
+    const booleanArray: GenericType<boolean> = [true, false, true]
+
+
+
+    const user: GenericType<{ name: string, age: number }> = [
+        {
+            name: "Humayun",
+            age: 20
+        },
+        {
+            name: "juyel",
+            age: 21
+        }
+    ]
+
+
+    // Generic Tuple
+
+    type GenericTuple<X, Y> = [X, Y]
+
+    // const mens: [string, string] = ['Mr. X', 'Mr. Y']
+    const mens: GenericTuple<string, string> = ['Mr. X', 'Mr. Y']
+
+    const userWithID: GenericTuple<number, { name: string, email: string }> = [1234, { name: "Humayun", email: "abc@gmail.com" }]
+
+
+
+
+    // 
+}
